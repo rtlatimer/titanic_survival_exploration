fcb3eaa6cfc609533f5bff358bdb87d153a80713
Rob Latimer <rtlatimer@gmail.com>
2017-04-14 16:54:04 -0700

    Add files via upload

---
+++ titanic_survival_exploration.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+
+# RMS Titanic data visualization code 
+from titanic_visualizations import survival_stats
+from IPython.display import display
+%matplotlib inline
